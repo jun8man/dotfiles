8c71b214c472439bd106b9074851f9b45c042897
junya <junya0220yamashita@gmail.com>
2016-01-18 21:35:16 +0530

    modify dotfiles

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,7 @@ ln -s ~/GitRepos/dotfiles/.vimrc ~/.vimrc
 ln -s ~/GitRepos/dotfiles/.vim ~/.vim
 ln -s ~/GitRepos/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/GitRepos/dotfiles/.emacs.d ~/.emacs.d
-ln -s ~/GitRepos/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/GitRepos/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s ~/GitRepos/dotfiles/git/.gitignore ~/.gitignore
+ln -s ~/GitRepos/dotfiles/.gemrc ~/.gemrc
 echo "finished."
